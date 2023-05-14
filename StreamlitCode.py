e1a00f315b6abbb177dd6fa20c35350e68ff2086
Manjunath Honamore <manjunath0honmore@gmail.com>
2023-05-14 16:18:43 +0530

    Update StreamlitCode.py

--- StreamlitCode.py
+++ StreamlitCode.py
@@ -83,5 +83,49 @@ def calculate(S,Yn,B,n,S0,Y):
     st.write("Critical depth = {:.2f}".format(Yc))
     st.write("Slope Type= ",stype)
     st.write("Curve Type= ",curve) 
+    diff=(abs(Yn-Yc))/10
+    flag=0
+    for i in range(11):
+        if flag==0:
+            flag=1
+            Yn=Yc
+        else:
+            Yn+=diff
+        
+        if shape=='Rectangle':
+            A = B * Yn
+            P = B + 2 * Yn
+            R = A / P
+            
+        elif shape=='Triangle':
+            A = S*(Yn**2)
+            P = (2 * Yn )* math.sqrt(1 +( S ** 2))
+            R = A / P
+            
+        else:
+            A = (B + S * Yn) * Yn
+            P = B + (2 * Yn) * (math.sqrt(1 +(S ** 2)))
+            R = A / P
+        V=Q/A
+        ## TO Store Prev Values In Variables
+        if i>0:
+            PE=E
+            PSf=Sf
+            if i>1:
+                PL=L
+        ## To Use Values Into Real Application
+        E=(Yn+(V**2)/(2*9.81))
+        Sf=((V*n)/(R**(2/3)))**2
+        if i>0:
+            DE=E-PE ## Delta E
+            MSf=((Sf+PSf)/2)     ## Mean Sf
+            S0_Sf=S0-MSf
+            DX=DE/S0_Sf
+            if i==1:
+                L=-DX
+            else:
+                L=PL-DX
+            #print(f'Area T={A} Wetted P={P} Hydraulic R={R} V={Q/A} E={E} Delta E={DE} Sf={Sf} Sf-{MSf} S0-Sf={S0_Sf} Delta X={DX} Length L={L}')    
+        st.write("Final Length After Iterations= ",L) 
 if st.sidebar.button("Calculate"):
     calculate(S,Yn,B,n,S0,Y)
